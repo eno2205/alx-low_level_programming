c5d721d5b9d793252943328ad56d11a75919c45b
eno2205 <enoeneh@gmail.com>
2022-07-05 08:16:11 -0700

    putchar ptinted

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/** main - prints putcher
+ * Return: always 0
+ */
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("_\n");
+
+	return (0);
